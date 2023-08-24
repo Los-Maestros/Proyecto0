cb4e2c06fa9941ba6badfbdacd886ca38b111bbc
Ilustre Daniel <123582748+danielfortizv@users.noreply.github.com>
2023-08-24 09:29:12 -0500

    Archivos

---
+++ task1.py
@@ -0,0 +1,118 @@
+#%%
+funciones = [
+    'jump', 'walk', 'leap', 'turn', 'turnto', 'drop', 'get', 'grab', 'letGo', 'nop', 'facing'
+]
+
+variables = [
+    'north', 'south', 'east', 'west'
+]
+
+extras = [
+    'if', 'else', 'while', 'repeat', 'times', 'can', 'not'
+]
+caracteres = ['{', '}', '(', ')', ';']
+
+def lector(file):
+    archivo = open(file, 'r')
+    texto = archivo.read().replace('\n',' ')
+    lista = lector2(texto)
+    return lista
+
+def llaves(archivo):
+    cont = 0
+    for i,j in enumerate(archivo):
+        if j == '{' or j == '(':
+            cont += 1
+        elif j == '}' or j == ')':
+            cont -= 1
+    if cont == 0:
+        return True
+    return False
+
+def lector2(texto):
+    respuesta = []
+    palabra = ''
+    for letra in texto:
+        if letra not in caracteres and letra != ' ':
+            palabra += letra
+        elif palabra != '':
+            respuesta.append(palabra)
+            if letra in caracteres:
+                respuesta.append(letra)
+            palabra = ''   
+    return respuesta
+
+def task(archivo):
+    while archivo:
+        palabra = archivo.pop(0)
+        
+        if palabra == 'defVar':
+            variable = archivo.pop(0)
+            variables.append(variable)
+        
+        elif palabra == 'defProc':
+            nombre = archivo.pop(0)
+            funciones.append(nombre)
+            archivo.pop(0)
+            parametros(archivo, variables)
+            
+        elif palabra in extras:
+            pass
+    
+def parametros(lista, var):
+    char = lista.pop(0)
+    while char != ')':
+        if char != ',':
+            var.append(char)
+        char = lista.pop(0)
+    return lista
+         
+    
+print(lector('Practica.txt'))
+
+
+
+# -------------------------------
+# EJEMPLO
+# -------------------------------
+
+def handle_go( u ):
+    print( "going %d units" % int(u))
+
+def handle_rotate( direc ):
+    print( "rotating %s" % direc.lower() )
+
+def handle_drill():
+    print( "drilling" )
+
+def find_matching_brace(body):
+    nest = 0
+    for i, n in enumerate(body):
+        if n == '{':
+            nest += 1
+        if n == '}':
+            if not nest:
+                return i
+            nest -= 1
+
+
+def process(body):
+    while body:
+        verb = body.pop(0)
+        if verb == "GO":
+            handle_go( body.pop(0) )
+            assert body.pop(0) == 'UNITS'
+        elif verb == "ROTATE":
+            handle_rotate(body.pop(0))
+        elif verb == "DRILL":
+            handle_drill()
+        elif verb == "REPEAT":
+            count = body.pop(0)
+            assert body.pop(0)=="TIMES"
+            assert body.pop(0)=="{"
+            closing = find_matching_brace(body)
+            newbody = body[0:closing]
+            print("repeat", count, closing, newbody)
+            for _ in range(int(count)):
+                process(newbody[:])
+            body = body[closing+1:]
